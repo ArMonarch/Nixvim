902c5294f98f7e97c186d51ce3c02a75c80ab4ae
ArMonarch <praffulthapa12@gmail.com>
2025-12-17 23:29:31 +0545

    feat: config for smear-cursor added

---
+++ neovim-config/lua/config/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  name = "smear-cursor.nvim",
+  event = "VeryLazy",
+  opts = {
+      stiffness = 0.5,
+      trailing_stiffness = 0.49,
+      never_draw_over_target = false,
+  },
+  specs = { },
+}
