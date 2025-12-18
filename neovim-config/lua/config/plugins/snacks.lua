49d9604ab085cd840bb9fc7c63a993a41bdab3ed
ArMonarch <praffulthapa12@gmail.com>
2025-12-18 15:24:17 +0545

    feat: changed layout for snacks picker and added file explorer plugins

--- neovim-config/lua/config/plugins/snacks.lua
+++ neovim-config/lua/config/plugins/snacks.lua
@@ -7,9 +7,12 @@ return {
 		bigfile = { enabled = false },
 		dashboard = { enabled = false },
 		explorer = { enabled = true },
-		indent = { enabled = false },
+		indent = { enabled = true },
 		input = { enabled = false },
-		picker = { enabled = false },
+		picker = {
+			enabled = true,
+			layout = { preset = "telescope" },
+		},
 		notifier = { enabled = false },
 		quickfile = { enabled = false },
 		scope = { enabled = false },
@@ -17,89 +20,38 @@ return {
 		statuscolumn = { enabled = false },
 		words = { enabled = false },
 	},
+  -- stylua: ignore
 	keys = {
 		-- snacks buffer delete keymaps
-		{
-			"<leader>ba",
-			function()
-				Snacks.bufdelete.all()
-			end,
-			desc = "Delete All Buffers",
-		},
-		{
-			"<leader>bd",
-			function()
-				Snacks.bufdelete.delete()
-			end,
-			desc = "Delete Current Buffer",
-		},
-		{
-			"<leader>bo",
-			function()
-				Snacks.bufdelete.other()
-			end,
-			desc = "Delete Other Buffers",
-		},
+		{	"<leader>ba", function() Snacks.bufdelete.all() end, desc = "Delete All Buffers" },
+		{	"<leader>bd", function() Snacks.bufdelete.delete() end, desc = "Delete Current Buffer" },
+		{	"<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete Other Buffers" },
 
 		-- snacks lazygit keymaps
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit({ cwd = Snacks.git.get_root() })
-			end,
-			desc = "Lazygit (Root Directory)",
-		},
-		{
-			"<leader>gG",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit (cwd)",
-		},
+		{	"<leader>gg", function() Snacks.lazygit({ cwd = Snacks.git.get_root() }) end, desc = "Lazygit (Root Directory)" },
+		{	"<leader>gG", function() Snacks.lazygit() end, desc = "Lazygit (cwd)" },
+
+    -- snacks picker -> misc keymaps
+		{	"<leader><leader>", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+
+    -- snacks picker -> files keymaps
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
 
 		-- snacks explorer keymaps
-		{
-			"<leader>e",
-			function()
-				Snacks.explorer({ cwd = Snacks.git.get_root() })
-			end,
-			desc = "Explorer Snacks (root dir)",
-		},
-		{
-			"<leader>E",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "Explorer Snacks (cwd)",
-		},
-		{
-			"<leader>fe",
-			function()
-				Snacks.explorer({ cwd = Snacks.git.get_root() })
-			end,
-			desc = "Explorer Snacks (root dir)",
-		},
-		{
-			"<leader>fE",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "Explorer Snacks (cwd)",
-		},
+		{	"<leader>e", function() Snacks.explorer({ cwd = Snacks.git.get_root() }) end, desc = "Explorer Snacks (root dir)" },
+		{	"<leader>E", function() Snacks.explorer() end, desc = "Explorer Snacks (cwd)" },
+		{	"<leader>fe", function() Snacks.explorer({ cwd = Snacks.git.get_root() }) end, desc = "Explorer Snacks (root dir)" },
+		{	"<leader>fE", function() Snacks.explorer() end, desc = "Explorer Snacks (cwd)" },
+
 		-- snacks notifications keymaps
-		{
-			"<leader>nh",
-			function()
-				Snacks.notifier.show_history()
-			end,
-			desc = "Notification History",
-		},
-		{
-			"<leader>nd",
-			function()
-				Snacks.notifier.hide()
-			end,
-			desc = "Dismiss All Notifications",
-		},
+		{	"<leader>nh", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+		{	"<leader>nd", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
 	},
 }
