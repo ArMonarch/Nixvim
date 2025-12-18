c71eb9ecec7e6f13db285b0a6f401b4daca41ebb
ArMonarch <praffulthapa12@gmail.com>
2025-12-18 17:42:30 +0545

    feat: integrated snacks.picker with todo-comments

--- neovim-config/lua/config/plugins/todo-comments.lua
+++ neovim-config/lua/config/plugins/todo-comments.lua
@@ -20,5 +20,19 @@ return {
 			end,
 			desc = "Previous Todo Comment",
 		},
+		{
+			"<leader>st",
+			function()
+				Snacks.picker.todo_comments()
+			end,
+			desc = "Todo",
+		},
+		{
+			"<leader>sT",
+			function()
+				Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+			end,
+			desc = "Todo/Fix/Fixme",
+		},
 	},
 }
