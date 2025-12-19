a05625eb2ae9facc19a7b1319a8c4850a7634343
ArMonarch <praffulthapa12@gmail.com>
2025-12-19 11:54:27 +0545

    feat: keymaps for diagnostic goto.

--- neovim-config/lua/config/keymaps.lua
+++ neovim-config/lua/config/keymaps.lua
@@ -89,6 +89,23 @@ map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
 -- folds keymaps
 map("n", "<leader>fo", "za", { desc = "Toggle Fold Under Cursor", remap = true })
 
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+	severity = severity and vim.diagnostic.severity[severity] or nil
+	return function()
+		go({ severity = severity })
+	end
+end
+-- stylua: ignore
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
+
 -- Delete Keymaps
 -- commenting
 vim.keymap.del("n", "gc")
