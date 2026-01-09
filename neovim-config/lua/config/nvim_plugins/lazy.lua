37a652621a6efdc77f62768d61cd6d10b20c229e
ArMonarch <praffulthapa12@gmail.com>
2026-01-09 16:44:41 +0545

    feat: added autocommand to handle java lsp attach on only java file insted of all buffers

--- neovim-config/lua/config/nvim_plugins/lazy.lua
+++ neovim-config/lua/config/nvim_plugins/lazy.lua
@@ -155,7 +155,10 @@ require("lazy").setup({
 	{ import = "config.nvim_plugins.which-key" },
 
 	-- configure jdtls for neovim
-	{ import = "config.nvim_plugins.nvim-jdtls" },
+	{
+		"mfussenegger/nvim-jdtls",
+		name = "nvim-jdtls",
+	},
 }, lazyOptions)
 
 -- set colorscheme
@@ -163,3 +166,47 @@ vim.cmd.colorscheme("catppuccin-mocha")
 
 -- load netrw file explorer as lazy.nvim disables it
 vim.cmd.packadd("netrw")
+
+-- setup jdtls to run on every java file with autocommand on FileType 'java'
+local function start_jtdls_client()
+	-- configure jdtls
+	local config = {
+		name = "jdtls",
+
+		-- `cmd` defines the executable to launch eclipse.jdt.ls.
+		-- `jdtls` must be available in $PATH and you must have Python3.9 for this to work.
+		--
+		-- As alternative you could also avoid the `jdtls` wrapper and launch
+		-- eclipse.jdt.ls via the `java` executable
+		-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+		cmd = { "jdtls" },
+
+		-- `root_dir` must point to the root of your project.
+		-- See `:help vim.fs.root`
+		root_dir = vim.fs.root(0, { "gradlew", ".git", "mvnw" }),
+
+		-- Here you can configure eclipse.jdt.ls specific settings
+		-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+		-- for a list of options
+		settings = {
+			java = {},
+		},
+
+		-- This sets the `initializationOptions` sent to the language server
+		-- If you plan on using additional eclipse.jdt.ls plugins like java-debug
+		-- you'll need to set the `bundles`
+		--
+		-- See https://codeberg.org/mfussenegger/nvim-jdtls#java-debug-installation
+		--
+		-- If you don't plan on any eclipse.jdt.ls plugins you can remove this
+		init_options = {
+			bundles = {},
+		},
+	}
+	require("jdtls").start_or_attach(config)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = start_jtdls_client,
+})
