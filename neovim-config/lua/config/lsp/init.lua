1e1d0a3a56f85c3e57c74eaf2dec4fcb7369824d
ArMonarch <praffulthapa12@gmail.com>
2026-01-26 16:10:25 +0545

    feat: updateed zig language server configuration.

--- neovim-config/lua/config/lsp/init.lua
+++ neovim-config/lua/config/lsp/init.lua
@@ -5,7 +5,6 @@ require("config.lsp.config.marksman")
 require("config.lsp.config.nil_ls")
 require("config.lsp.config.nixd")
 require("config.lsp.config.ts_ls")
-require("config.lsp.config.zls")
 
 -- NOTE: Enable Language Servers here,
 -- needs Neovim v0.11+, as it used functions available on Neovim version >= 0.11
@@ -16,7 +15,6 @@ vim.lsp.enable("marksman")
 vim.lsp.enable("nil_ls")
 vim.lsp.enable("nixd")
 vim.lsp.enable("ts_ls")
-vim.lsp.enable("zls")
 
 -- Enable Inlay Hints
 vim.lsp.inlay_hint.enable(true)
@@ -46,3 +44,5 @@ require("config.lsp.jdtls")
 require("config.lsp.rust-analyzer")
 -- import the configuration for texlab
 require("config.lsp.texlab")
+-- import the configuration for zig language server
+require("config.lsp.zls")
---
+++ neovim-config/lua/config/lsp/zls.lua
@@ -0,0 +1,46 @@
+---@brief
+--- https://github.com/zigtools/zls
+---
+--- Zig LSP implementation + Zig Language Server
+
+---@type vim.lsp.Config
+local zls_config = {
+	name = "zls",
+	cmd = { "zls" },
+	filetypes = { "zig", "zir" },
+	root_markers = { "zls.json", "build.zig", ".git" },
+	workspace_required = false,
+}
+
+-- add the zig language server configuration
+vim.lsp.config("zls", zls_config)
+
+local run_zls = function()
+	-- check if zig language server is installed or in path
+	if vim.fn.executable("zls") == 0 then
+		vim.notify("The language server `zls` is either not installed, missing from PATH, or not executable.", "error")
+		return
+	end
+
+	-- check if zig language server is installed or in path
+	if vim.fn.executable("zig") == 0 then
+		vim.notify(
+			"The language server `zls` requires `zig which is either not installed, missing from PATH, or not executable.",
+			"error"
+		)
+		return
+	end
+
+	-- return if client is already connected to buffer
+	if vim.lsp.get_clients({ name = "zls" })[1] then
+		return
+	end
+
+	vim.lsp.enable("zls", true)
+end
+
+-- setup rust-analyzer to run on every rust file with autocommand on FileType 'rust'
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "zig", "zir" },
+	callback = run_zls,
+})
